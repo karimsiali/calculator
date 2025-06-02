d8272e66541cf245f34a2830d34b578c3679bb84
Karim <52040753+karimsiali@users.noreply.github.com>
2025-06-02 07:59:49 +0100

    Add support for negative numbers
    I added option to operate with negative numbers

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let firstNumber, secondNumber, operator;
 let displayFull = document.querySelector(".full");
 let displayCurrent = document.querySelector(".current");
 let buttons = document.querySelector(".buttons");
@@ -7,8 +6,10 @@ let operators = document.querySelectorAll(".operator");
 let result = document.querySelector(".result");
 let clearBtn = document.querySelector(".clear");
 let deleteBtn = document.querySelector(".back");
+let negative = document.querySelector(".negative");
 let dot = document.querySelector(".dot");
 let dotPressed = false;
+let negativeStatus = false;
 
 function add(a, b) {
     return Math.round((a + b) * (Math.pow(10, 10))) / (Math.pow(10, 10));
@@ -61,9 +62,11 @@ function takeInput() {
 
     clearBtn.addEventListener("click", clear);
 
-    dot.addEventListener("click", typeDot)
+    dot.addEventListener("click", typeDot);
 
-    deleteBtn.addEventListener("click", deleteDigit)
+    deleteBtn.addEventListener("click", deleteDigit);
+
+    negative.addEventListener("click", changeSign);
     
     document.addEventListener("keydown", (e) => {
         let key = e.key;
@@ -102,44 +105,56 @@ function takeInput() {
 
     function typeOperator(e) {
         dotPressed = false;
-            totalNumbers = 0;            
-            try {
-                key = e.target.textContent;
-            } catch (err) {
-                key = e;
+        totalNumbers = 0;            
+        try {
+            key = e.target.textContent;
+        } catch (err) {
+            key = e;
+        }
+        if (!firstNumber && !selectedOperator) {
+            firstNumber = displayCurrent.textContent;
+            if (negativeStatus == true) {
+                firstNumber *= - 1;
+                changeSign();
             }
-            if (!firstNumber && !selectedOperator) {
-                firstNumber = displayCurrent.textContent;
-                selectedOperator = key;
-                displayFull.textContent += selectedOperator;
-                displayCurrent.textContent = "";
-                
-            } else if (firstNumber && selectedOperator && !displayResult) {
-                secondNumber = displayCurrent.textContent;                              
-                if (+secondNumber != 0) {
-                    displayResult = operate(selectedOperator, +firstNumber, +secondNumber);                  
-                    displayFull.textContent = displayResult + key;
-                    firstNumber = displayResult;
-                    displayCurrent.textContent = "";
-                    selectedOperator = key;
-                    displayResult = null;
-                } else {   
-                    displayCurrent.textContent = "";
-                    displayFull.textContent = firstNumber + key;
-                    selectedOperator = key;
+            selectedOperator = key;
+            displayFull.textContent = firstNumber + selectedOperator;
+            displayCurrent.textContent = "";
+            
+        } else if (firstNumber && selectedOperator && !displayResult) {
+            secondNumber = displayCurrent.textContent;                              
+            if (+secondNumber != 0) {
+                if (negativeStatus == true) {
+                    secondNumber *= - 1;
+                    changeSign();
                 }
-                
-            } else if (!displayResult && firstNumber != 0) {                
+                displayResult = operate(selectedOperator, +firstNumber, +secondNumber);                  
+                displayFull.textContent = displayResult + key;
+                firstNumber = displayResult;
+                displayCurrent.textContent = "";
                 selectedOperator = key;
-                displayFull.textContent = firstNumber + selectedOperator;
-                
-            } else {
                 displayResult = null;
-                firstNumber = displayCurrent.textContent;
+            } else {   
+                displayCurrent.textContent = "";
+                displayFull.textContent = firstNumber + key;
                 selectedOperator = key;
-                displayFull.textContent = firstNumber + selectedOperator;
-                displayCurrent.textContent = "";                
             }
+            
+        } else if (!displayResult && firstNumber != 0) {                
+            selectedOperator = key;
+            displayFull.textContent = firstNumber + selectedOperator;
+            
+        } else {
+            displayResult = null;
+            firstNumber = displayCurrent.textContent;
+            if (negativeStatus == true) {
+                firstNumber *= - 1;
+                changeSign();
+            }
+            selectedOperator = key;
+            displayFull.textContent = firstNumber + selectedOperator;
+            displayCurrent.textContent = "";                
+        }
     }
 
     function showResult() {
@@ -149,15 +164,20 @@ function takeInput() {
             displayResult = +(displayCurrent.textContent);
             displayCurrent.textContent = displayResult;
             dotPressed = false;
+            negativeStatus = false;
         } else {         
             secondNumber = displayCurrent.textContent;
+            if (negativeStatus == true) {
+                secondNumber *= - 1;
+                changeSign();
+            }
             displayResult = operate(selectedOperator, +firstNumber, +secondNumber);
             if (displayResult == "ERROR") {
                 clear();
                 displayCurrent.textContent = "ERROR";
             } else {
                 displayCurrent.textContent = displayResult;
-            }            
+            }      
         }
     }
 
@@ -188,6 +208,19 @@ function takeInput() {
         displayCurrent.innerHTML = displayFull.innerHTML = "";
         dotPressed = false;
         totalNumbers = 0;
+        if (negativeStatus == true) {
+            changeSign();
+        }        
+    }
+    
+    function changeSign() {
+        if (negativeStatus == false) {
+            negativeStatus = true;
+            document.querySelector(".sign").textContent = "-"
+        } else {
+            negativeStatus = false;
+            document.querySelector(".sign").textContent = ""
+        }
     }
 }
 
